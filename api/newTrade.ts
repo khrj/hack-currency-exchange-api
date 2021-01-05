30fd91413c394c4a8a46587dd173eb6e4e33c67d
Khushraj Rathod <khushraj.rathod@gmail.com>
2021-01-05 16:42:30 +0530

    Add new trade implementation for giving HN

--- api/newTrade.ts
+++ api/newTrade.ts
@@ -1,8 +1,9 @@
+import { Validity, validateToken } from './validateToken'
+
 import { NowRequest, NowResponse } from '@vercel/node'
+import axios from 'axios'
 import { PrismaClient, TradeStatus, Currency, FromOrTo } from '@prisma/client'
 const prisma = new PrismaClient()
-
-import { Validity, validateToken } from './validateToken'
 
 export default async function (request: NowRequest, response: NowResponse) {
     let input
@@ -15,7 +16,7 @@ export default async function (request: NowRequest, response: NowResponse) {
     const valid = await validateToken(input.token)
 
     if (valid !== Validity.VALID && valid !== Validity.ADMIN) {
-        return response.status(400).send("Unauthorized: Token not valid")
+        return response.status(403).send("Unauthorized: Token not valid")
     }
 
     try {
@@ -25,9 +26,45 @@ export default async function (request: NowRequest, response: NowResponse) {
             }
         })
 
-        // TODO interact with Currency API
+        const queryData = `
+            mutation ($amount: Float!, $user: String!) {
+	            transact (data: {
+		            from: $user,
+		            to: "U01JD2MBVUY",
+		            balance: $amount,
+                    for: "Offer trade in the Hack Currency Exchange" 
+                }) { 
+                    id 
+                }
+            }
+        `
 
-        const created = await prisma.trade.create({
+        let incompleteTransactionID: number
+
+        switch (input.from) {
+            case Currency.HN:
+                const response = await axios.post('https://hn.rishi.cx', {
+                    query: queryData.replace(/\s/g, ''),
+                    variables: {
+                        amount: input.currencyWhichIsOne === FromOrTo.FROM ? input.units : input.units * input.amountPerUnit,
+                        user: tokenObject!.userID
+                    }
+                }, {
+                    headers: {
+                        secret: process.env.HN_SECRET!
+                    }
+                })
+
+                const body = response.data
+                incompleteTransactionID = parseInt(body.data.transact.id)
+                break
+            case Currency.GP:
+                // TODO
+                incompleteTransactionID = -1
+                break
+        }
+
+        await prisma.trade.create({
             data: {
                 from: input.from,
                 to: input.to,
@@ -40,12 +77,14 @@ export default async function (request: NowRequest, response: NowResponse) {
                     connect: {
                         token_id: tokenObject?.token_id
                     }
-                }
+                },
+                incompleteTransactionID
             }
         })
 
-        return response.status(201).send(created.trade_id)
+        return response.status(201).send(incompleteTransactionID)
     } catch (e) {
+        console.error(e)
         return response.status(500).send("Unexpected error while creating trade")
     }
 }
