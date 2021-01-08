9b9fdf8d894c3a534c9ee594dad3ef87be126aac
Khushraj Rathod <khushraj.rathod@gmail.com>
2021-01-08 16:59:31 +0530

    Split listTrades > listCompletedTrades & listActiveTrades, write latter

---
+++ api/listActiveTrades.ts
@@ -0,0 +1,44 @@
+import { NowRequest, NowResponse } from '@vercel/node'
+import { PrismaClient, TradeStatus, Trade } from '@prisma/client'
+import { Validity, validateToken } from './validateToken'
+import _ from 'lodash'
+const prisma = new PrismaClient()
+
+export default async (request: NowRequest, response: NowResponse) => {
+    try {
+        const valid = await validateToken(request.headers.token as string)
+
+        if (valid !== Validity.VALID && valid !== Validity.ADMIN) {
+            return response.status(403).send("Unauthorized: Token not valid")
+        }
+    } catch (e) {
+        return response.status(400).send(e)
+    }
+
+    const trades = await prisma.trade.findMany({
+        where: {
+            status: TradeStatus.IN_PROGRESS
+        }
+    })
+
+    const toReturn = trades.map(trade => {
+        return _.pick(trade,
+            [
+                'sellerID',
+                'units',
+                'from',
+                'to',
+                'currencyWhichIsOne',
+                'amountPerUnit',
+                'unitsRemaining'
+            ])
+    })
+
+    response.status(200).send(JSON.stringify(toReturn))
+}
+
+// await fetch("https://exchange.khushrajrathod.com/api/listActiveTrades", {
+//     headers: {
+//         token: "<TOKEN>",
+//     }
+// })
