f79147cf0d9bb7738c8d14d29baa9bd64784b86d
Khushraj Rathod <khushraj.rathod@gmail.com>
2021-01-08 17:58:42 +0530

    Add method to list completed trades

--- api/listCompletedTrades.ts
+++ api/listCompletedTrades.ts
@@ -1,7 +1,44 @@
 import { NowRequest, NowResponse } from '@vercel/node'
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, TradeStatus } from '@prisma/client'
+import { Validity, validateToken } from './validateToken'
+import _ from 'lodash'
 const prisma = new PrismaClient()
 
-export default (request: NowRequest, response: NowResponse) => {
-    response.status(501).send(`Not Implemented`)
+export default async (request: NowRequest, response: NowResponse) => {
+    try {
+        const valid = await validateToken(request.headers.token as string)
+
+        if (valid !== Validity.VALID && valid !== Validity.ADMIN) {
+            return response.status(403).send("Unauthorized: Token not valid")
+        }
+    } catch (e) {
+        return response.status(400).send(e)
+    }
+
+    const trades = await prisma.trade.findMany({
+        where: {
+            status: TradeStatus.COMPLETED
+        }
+    })
+
+    const toReturn = trades.map(trade => {
+        return _.pick(trade,
+            [
+                'sellerID',
+                'units',
+                'from',
+                'to',
+                'currencyWhichIsOne',
+                'amountPerUnit',
+                'unitsRemaining'
+            ])
+    })
+
+    response.status(200).send(JSON.stringify(toReturn))
 }
+
+// await fetch("https://exchange.khushrajrathod.com/api/listActiveTrades", {
+//     headers: {
+//         token: "<TOKEN>",
+//     }
+// })
